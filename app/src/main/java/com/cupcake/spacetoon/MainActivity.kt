aa864d00a315fcca4443ce8849c4e8a855443ce4
ameeramjed <pr.ameer98@gmail.com>
2023-03-11 01:57:45 +0300

    clean up

--- app/src/main/java/com/cupcake/spacetoon/MainActivity.kt
+++ app/src/main/java/com/cupcake/spacetoon/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
+//        setContentView(R.layout.profile_screen)
+//        setContentView(R.layout.game_screen)
     }
 }
